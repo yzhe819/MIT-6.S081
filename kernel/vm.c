3251bdc62ed97dc98dacc7d8e32587fe3bc0ef90
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-06-24 18:07:45 +1200

    add: copyinstr_new -> copyinstr

--- kernel/vm.c
+++ kernel/vm.c
@@ -420,40 +420,8 @@ copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
 int
 copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 {
-  uint64 n, va0, pa0;
-  int got_null = 0;
-
-  while(got_null == 0 && max > 0){
-    va0 = PGROUNDDOWN(srcva);
-    pa0 = walkaddr(pagetable, va0);
-    if(pa0 == 0)
-      return -1;
-    n = PGSIZE - (srcva - va0);
-    if(n > max)
-      n = max;
-
-    char *p = (char *) (pa0 + (srcva - va0));
-    while(n > 0){
-      if(*p == '\0'){
-        *dst = '\0';
-        got_null = 1;
-        break;
-      } else {
-        *dst = *p;
-      }
-      --n;
-      --max;
-      p++;
-      dst++;
-    }
-
-    srcva = va0 + PGSIZE;
-  }
-  if(got_null){
-    return 0;
-  } else {
-    return -1;
-  }
+  // the source code is replaced by the copyinstr_new method in vmcopyin.c
+  return copyinstr_new(pagetable, dst, srcva, max);
 }
 
 void
