70a9d19f667b4dd20399e9c70053fb0403a8d9d8
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-07-04 01:28:28 +1200

    add: backtrace for lab four

--- kernel/printf.c
+++ kernel/printf.c
@@ -118,6 +118,7 @@ void
 panic(char *s)
 {
   pr.locking = 0;
+  backtrace();
   printf("panic: ");
   printf(s);
   printf("\n");
@@ -131,4 +132,16 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  printf("backtrace:\n");
+  uint64 fp = r_fp();
+  uint64 stack_top = PGROUNDUP(fp);
+  while (fp != stack_top) {
+    printf("%p\n",*(uint64*)(fp - 8));
+    fp = *(uint64*)(fp - 16);
+  }
 }
