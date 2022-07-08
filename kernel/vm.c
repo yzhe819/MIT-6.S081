070368ae66ff3188f0af7a373d61156e7522bcf1
yzhe819 <yzhe819@aucklanduni.ac.nz>
2022-07-09 03:08:52 +1200

    fix: read failed

--- kernel/vm.c
+++ kernel/vm.c
@@ -362,7 +362,7 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
 
-    if(cowpage(pagetable, va0) == 0)
+    if(cowpage(pagetable, va0))
       pa0 = (uint64)cowalloc(pagetable, va0);
 
     if(pa0 == 0)
